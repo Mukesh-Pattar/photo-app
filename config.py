52e1d872a839a67fb7122239c1b025cf617f6f87
Mukesh-Pattar <95522153+Mukesh-Pattar@users.noreply.github.com>
2025-12-10 18:00:09 +0530

    Hardcode AWS bucket name and region
    Removed environment variable retrieval for AWS settings.

--- config.py
+++ config.py
@@ -1,4 +1,2 @@
-import os
-
-AWS_BUCKET_NAME = os.getenv("AWS_BUCKET_NAME","photo-app-bucket-mukesh0902")
-AWS_REGION = os.getenv("AWS_REGION", "ap-south-1")
+AWS_BUCKET_NAME = "photo-app-bucket-mukesh0902"
+AWS_REGION = "ap-south-1"   # Change if your bucket is in another region
